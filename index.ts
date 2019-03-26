4d3203f398d71b9e7cc05dde0f450cc90f14b1f1
Afonso F <afonso.franca@gmail.com>
2019-03-25 22:37:04 -0300

    actionFactory

--- index.ts
+++ index.ts
@@ -1,30 +1,78 @@
 import {Request, Response, NextFunction} from "express";
 import {validationResult} from "express-validator/check";
+import * as Raven from 'raven';
 
 export interface IActionParams {
     req: Request,
-        res: Response,
-        next: NextFunction,
+    res: Response,
+    next: NextFunction,
 }
 
 type ActionFn = (actionParams: IActionParams) => Promise<any>;
 
-export const action = (actionFn: ActionFn) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({errors: errors.array()});
-        }
-        return actionFn({req, res, next})
-            .then((obj: any) => {
-                if (obj === null) {
-                    res.status(404).send('Not found');
-                } else {
-                    res.json(obj);
-                }
-            })
-            .catch((err) => {
-                next(err);
-            });
+const handle200 = (req, res, next, data) => {
+    res.status(200).json(data);
+};
+
+const handle404 = (req, res, next) => {
+    res.status(404).json({
+        message: 'Not found'
+    });
+};
+
+const handle400 = (req, res, next, data) => {
+    res.status(400).json({
+        message: 'Bad Request',
+        data,
+    });
+};
+
+const handle500 = (req, res, next, err) => {
+    try {
+        raven.captureException(err);
+    } catch (sentryErr) {
+        console.debug('unable to use Sentry on error: ', sentryErr);
+    }
+    next(err);
+};
+
+let raven;
+
+function startRaven({config}) {
+    const xxx = {
+        release: config.package.version,
+        environment: config.env,
+        autoBreadcrumbs: true,
+        captureUnhandledRejections: true,
+        tags: {
+            platform: config.package.name,
+        },
     };
+
+    if (!raven) {
+        Raven.config(config.sentry.dsn, xxx).install();
+        raven = Raven;
+    }
+}
+
+export const actionFactory = ({config}) => {
+    startRaven({config});
+    return (actionFn: ActionFn) =>
+        (req: Request, res: Response, next: NextFunction) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return handle400(req, res, next, errors.array());
+            }
+            return actionFn({req, res, next})
+                .then((data: any) => {
+                    if (data === null) {
+                        handle404(req, res, next);
+                    } else {
+                        handle200(req, res, next, data);
+                    }
+                })
+                .catch((err) => {
+                    handle500(req, res, next, err);
+                });
+        };
 };
